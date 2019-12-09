7fd308eac07a58c434d2bd69a3d55049fd52f269
Luuk Vermeer <l.vermeer@student.fontys.nl>
2019-12-09 15:45:22 +0100

    Tried to make Login Work

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  InputEmail1: string;
+  InputPassword1: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  signIn() {
+    console.log(this.InputEmail1);
+    console.log(this.InputPassword1);
   }
 
 }
