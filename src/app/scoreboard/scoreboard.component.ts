558a67d0e2517d370abac6de7edacf925d2f7a70
Vermeer,Luuk L.L.H <l.vermeer@student.fontys.nl>
2020-01-14 17:20:23 +0100

    Added Scoreboard.

---
+++ src/app/scoreboard/scoreboard.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AccountcompetitionService} from '../shared/serv/accountcompetition/accountcompetition.service';
+import {AccountCompetition} from '../shared/models/AccountCompetition';
+
+@Component({
+  selector: 'app-scoreboard',
+  templateUrl: './scoreboard.component.html',
+  styleUrls: ['./scoreboard.component.css']
+})
+export class ScoreboardComponent implements OnInit {
+
+  id: number;
+  participants: AccountCompetition[];
+  constructor(private route: ActivatedRoute, private service: AccountcompetitionService) {
+    this.route.params.subscribe(params => {
+      this.id = params.id;
+    });
+  }
+
+  ngOnInit() {
+    this.service.getAccountCompetitions().subscribe(data => {
+      console.log(data);
+      this.participants = data;
+    });
+  }
+
+}
